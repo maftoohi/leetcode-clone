591115c9653a5b5cf10f8c9abf9865c05faf8949
Nafise Maftoohi <nafise@nafise.local>
2023-06-01 11:07:55 +0100

    add reset password logic

--- src/components/Modals/RessetPassword.tsx
+++ src/components/Modals/RessetPassword.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
 
 type RessetpasswordProps = {};
 
 const Ressetpassword: React.FC<RessetpasswordProps> = () => {
+  const [email, setEmail] = useState("");
+  const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+  const handleReset = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const success = await sendPasswordResetEmail(email);
+    if (success) {
+      alert("Sent email");
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert(error.message);
+    }
+  }, [error]);
+
   return (
-    <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8">
+    <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" onSubmit={handleReset}>
       <h3 className="text-xl font-medium  text-white">Reset Password</h3>
       <p className="text-sm text-white ">Forgotten your password? Enter your e-mail address below, and we&apos;ll send you an e-mail allowing you to reset it.</p>
       <div>
         <label htmlFor="email" className="text-sm font-medium block mb-2 text-gray-300">
           Your email
         </label>
-        <input type="email" name="email" id="email" className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder="name@company.com" />
+        <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder="name@company.com" />
       </div>
       <button
         type="submit"
