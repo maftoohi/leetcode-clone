e8e754e15074bb99c6a6a2cc286b658f539edbc9
Nafise Maftoohi <nafise@nafise.local>
2023-06-02 15:31:02 +0100

    create Workspace component

---
+++ src/components/Workspace/Playground/Playground.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PreferenceNav from "./PreferenceNav/PreferenceNav";
+
+type PlaygroundProps = {};
+
+const Playground: React.FC<PlaygroundProps> = () => {
+  return (
+    <>
+      <PreferenceNav />
+    </>
+  );
+};
+export default Playground;
