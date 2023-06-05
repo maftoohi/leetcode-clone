f5d4bff1315cb4163d36f5cb6b58e5db0e04fb10
Nafise Maftoohi <nafise@nafise.local>
2023-06-05 21:21:48 +0100

    add SSG

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,15 +1,55 @@
 import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
+import { problems } from "@/utils/problems";
+import { Problem } from "@/utils/types/problem";
 import React from "react";
 
-type ProblemPageProps = {};
+type ProblemPageProps = {
+  problem: Problem;
+};
 
-const ProblemPage: React.FC<ProblemPageProps> = () => {
+const ProblemPage: React.FC<ProblemPageProps> = ({ problem }) => {
+  console.log(problem);
   return (
     <div>
       <Topbar problemPage />
-      <Workspace />
+      <Workspace problem={problem} />
     </div>
   );
 };
 export default ProblemPage;
+
+//fetch the local data.
+//SSG
+//getStaticPaths => creates the dynamic routes.
+
+export async function getStaticPaths() {
+  const paths = Object.keys(problems).map((key) => ({
+    params: { pid: key },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+//getStaticProps => fetches the data
+
+export async function getStaticProps({ params }: { params: { pid: string } }) {
+  const { pid } = params;
+  const problem = problems[pid];
+
+  if (!problem) {
+    return {
+      notFound: true,
+    };
+  }
+  problem.handlerFunction = problem.handlerFunction.toString();
+
+  return {
+    props: {
+      problem,
+    },
+  };
+}
