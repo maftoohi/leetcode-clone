22d674af853ec1fe18b86d1767d97bef8f90c487
Nafise Maftoohi <nafise@nafise.local>
2023-06-05 16:35:49 +0100

    update imports

--- src/utils/problems/two-sum.ts
+++ src/utils/problems/two-sum.ts
@@ -1,23 +1,5 @@
 import assert from "assert";
-
-export type Example = {
-  id: number;
-  inputText: string;
-  outputText: string;
-  explanation?: string;
-  img?: string;
-};
-export type Problem = {
-  id: string;
-  title: string;
-  problemStatement: string;
-  examples: Example[];
-  constraints: string;
-  order: number;
-  starterCode: string;
-  handlerFunction: ((fn: any) => boolean) | string;
-  starterFunctionName: string;
-};
+import { Problem } from "../types/problem";
 
 const starterCodeTwoSum = `function twoSum(nums,target){
     // Write your code here
