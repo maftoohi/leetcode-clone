4372752e761d2f1871e49d6bd3feca53bb9d755c
Nafise Maftoohi <nafise@nafise.local>
2023-06-05 21:56:17 +0100

    pass problem to PlayGround as props

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -12,7 +12,7 @@ const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
   return (
     <Split className="split" minSize={0}>
       <ProblemDescription problem={problem} />
-      <Playground />
+      <Playground problem={problem} />
     </Split>
   );
 };
