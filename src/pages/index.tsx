b6e66f9d826e04d87d5948a2cd0d79de6a2cd01c
Nafise Maftoohi <nafise@nafise.local>
2023-06-06 17:45:48 +0100

    add loading skeleton UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,9 @@
 import Topbar from "@/components/Topbar/Topbar";
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import { useState } from "react";
-import { doc, setDoc } from "firebase/firestore";
-import { firestore } from "@/firebase/firebase";
 
 export default function Home() {
-  const [inputs, setInputs] = useState({
-    id: "",
-    title: "",
-    difficulty: "",
-    category: "",
-    videoId: "",
-    link: "",
-    order: 0,
-    likes: 0,
-    dislikes: 0,
-  });
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    //convert inputs.number to integer
-    const newProblem = {
-      ...inputs,
-      order: Number(inputs.order),
-    };
-    await setDoc(doc(firestore, "problems", inputs.id), newProblem);
-    alert("saved to db");
-  };
+  const [loadingProblems, setLoadingProblems] = useState(true);
 
   return (
     <main className="bg-dark-layer-2 min-h-screen">
@@ -42,42 +15,51 @@ export default function Home() {
         &ldquo; Challenges &ldquo;
       </h1>
       <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+        {loadingProblems && (
+          <div className="animate-pulse max-w-[1200px] mx-auto sm:w-7/12 w-full">
+            {[...Array(10)].map((_, idx) => (
+              <LoadingSkeleton key={idx} />
+            ))}
+          </div>
+        )}
         <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
-          <thead className="text-xs uppercase text-gray-400 border-b ">
-            <tr>
-              <th scope="col" className="px-1 py-3 w-0 font-medium">
-                Status
-              </th>
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Title
-              </th>
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Difficulty
-              </th>
+          {!loadingProblems && (
+            <thead className="text-xs uppercase text-gray-400 border-b ">
+              <tr>
+                <th scope="col" className="px-1 py-3 w-0 font-medium">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Title
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Difficulty
+                </th>
 
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Solution
-              </th>
-            </tr>
-          </thead>
-          <ProblemsTable />
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Solution
+                </th>
+              </tr>
+            </thead>
+          )}
+          <ProblemsTable setLoadingProblems={setLoadingProblems} />
         </table>
       </div>
-
-      {/*temp form*/}
-      <form className="p-6 flex flex-col max-w-sm gap-3" onSubmit={handleSubmit}>
-        <input onChange={handleInputChange} type="text" placeholder="title" name="title" />
-        <input onChange={handleInputChange} type="text" placeholder="problem id" name="id" />
-        <input onChange={handleInputChange} type="text" placeholder="difficulty" name="difficulty" />
-        <input onChange={handleInputChange} type="text" placeholder="category" name="category" />
-        <input onChange={handleInputChange} type="text" placeholder="order" name="order" />
-        <input onChange={handleInputChange} type="text" placeholder="videoId?" name="videoId" />
-        <input onChange={handleInputChange} type="text" placeholder="link?" name="link" />
-        <button className="bg-white">Save to db</button>
-      </form>
     </main>
   );
 }
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex items-center space-x-12 mt-4 px-6">
+      <div className="w-6 h-6 shrink-0 rounded-full bg-dark-layer-1"></div>
+      <div className="h-4 sm:w-52  w-32  rounded-full bg-dark-layer-1"></div>
+      <div className="h-4 sm:w-52  w-32 rounded-full bg-dark-layer-1"></div>
+      <div className="h-4 sm:w-52 w-32 rounded-full bg-dark-layer-1"></div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
