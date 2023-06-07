ead93ad1b889fafe38f6074f6d44462adc36bf7c
Nafise Maftoohi <nafise@nafise.local>
2023-06-07 11:19:24 +0100

    add logic for creating users in DB

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
-import { auth } from "@/firebase/firebase";
+import { auth, firestore } from "@/firebase/firebase";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
+import { doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 type SignupProps = {};
 
@@ -28,11 +30,28 @@ const Signup: React.FC<SignupProps> = () => {
     e.preventDefault();
     if (!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields");
     try {
+      toast.loading("Creating your account", { position: "top-center", toastId: "loadingToast" });
       const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
       if (!newUser) return;
+
+      const userData = {
+        uid: newUser.user.uid,
+        email: newUser.user.email,
+        displayName: inputs.displayName,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        likedProblems: [],
+        dislikedProblems: [],
+        solvedProblems: [],
+        starredProblem: [],
+      };
+      await setDoc(doc(firestore, "users", newUser.user.uid), userData);
+
       router.push("/");
     } catch (error: any) {
-      alert(error.message);
+      toast.error(error.message, { position: "top-center" });
+    } finally {
+      toast.dismiss("loadingToast");
     }
   };
 
