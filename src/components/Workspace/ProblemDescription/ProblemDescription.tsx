0d28f091fcb1e5e231d39c7599be18408754e24b
Nafise Maftoohi <nafise@nafise.local>
2023-06-07 16:50:41 +0100

    get user data on problem

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -4,10 +4,11 @@ import { BsCheck2Circle } from "react-icons/bs";
 import { TiStarOutline } from "react-icons/ti";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { firestore } from "@/firebase/firebase";
+import { auth, firestore } from "@/firebase/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import CircleSkeleton from "@/components/Skeletons/CircleSkeleton";
 import RectangleSkeleton from "@/components/Skeletons/RectangleSkeleton";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type ProblemDescriptionProps = {
   problem: Problem;
@@ -123,4 +124,31 @@ const useGetCurrentProblem = (problemId: string) => {
   }, [problemId]);
 
   return { currentProblem, loading, problemDifficultyClass };
+};
+
+const useGetUsersDataOnProblem = (problemId: string) => {
+  const [data, setData] = useState({ liked: false, disliked: false, starred: false, solved: false });
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    const getUsersDataOnProblem = async () => {
+      const userRef = doc(firestore, "users", user!.uid);
+      const userSnap = await getDoc(userRef);
+      if (userSnap.exists()) {
+        const data = userSnap.data();
+        const { solvedProblems, likedProblems, dislikedProblems, starredProblems } = data;
+        setData({
+          liked: likedProblems.includes(problemId),
+          disliked: dislikedProblems.includes(problemId),
+          starred: starredProblems.includes(problemId),
+          solved: solvedProblems.includes(problemId),
+        });
+      }
+    };
+
+    if (user) getUsersDataOnProblem();
+    return () => setData({ liked: false, disliked: false, starred: false, solved: false });
+  }, [problemId, user]);
+
+  return { ...data, setData };
 };
