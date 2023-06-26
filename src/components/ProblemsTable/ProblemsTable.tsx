5237e9cd63ab0c7c67b9a4200b18dcbe66b9b2b0
Nafise Maftoohi <nafise@nafise.local>
2023-06-26 11:55:03 +0100

    update Homepage

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -4,9 +4,10 @@ import { AiFillYoutube } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
 import YouTube from "react-youtube";
 import Link from "next/link";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
-import { firestore } from "@/firebase/firebase";
+import { collection, doc, getDoc, getDocs, orderBy, query } from "firebase/firestore";
+import { auth, firestore } from "@/firebase/firebase";
 import { DBProblem } from "@/utils/types/problem";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type ProblemsTableProps = {
   setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
@@ -19,6 +20,7 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => 
   });
 
   const problems = useGetProblems(setLoadingProblems);
+  const solvedProblems = useGetSolvedProblem();
 
   const closeModal = () => {
     setYoutubePlayer({ isOpen: false, videoId: "" });
@@ -30,9 +32,7 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => 
           const difficulyColor = problem.difficulty === "Easy" ? "text-dark-green-s" : problem.difficulty === "Medium" ? "text-dark-yellow" : "text-dark-pink";
           return (
             <tr className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`} key={problem.id}>
-              <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
-                <BsCheckCircle fontSize={18} />
-              </th>
+              <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">{solvedProblems.includes(problem.id) && <BsCheckCircle fontSize={18} />}</th>
               <td className="px-6 py-4">
                 {problem.link ? (
                   <Link href={problem.link} className="hover:text-blue-600 cursor-pointer" target="_blank">
@@ -87,4 +87,23 @@ const useGetProblems = (setLoadingProblems: React.Dispatch<React.SetStateAction<
     getProblems();
   }, [setLoadingProblems]);
   return problems;
+};
+
+const useGetSolvedProblem = () => {
+  const [solvedProblems, setSolvedProblem] = useState<string[]>([]);
+  const [user] = useAuthState(auth);
+  useEffect(() => {
+    const getSolvedProblems = async () => {
+      const userRef = doc(firestore, "users", user!.uid);
+      const userDoc = await getDoc(userRef);
+
+      if (userDoc.exists()) {
+        setSolvedProblem(userDoc.data().solvedProblems);
+      }
+    };
+    if (user) getSolvedProblems();
+    if (!user) setSolvedProblem([]);
+  }, [user]);
+
+  return solvedProblems;
 };
