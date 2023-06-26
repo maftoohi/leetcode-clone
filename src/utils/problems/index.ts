70451d25ddfda3e7e3c9b6d580b4ca1377cdbe2b
Nafise Maftoohi <nafise@nafise.local>
2023-06-26 18:35:34 +0100

    correct import statement

--- src/utils/problems/index.ts
+++ src/utils/problems/index.ts
@@ -1,7 +1,7 @@
 import { Problem } from "../types/problem";
 import { jumpGame } from "./jump-game";
 import { reverseLinkedList } from "./reverse-linked-list";
-import { search2DMatrix } from "./search-a-2D-matrix";
+import { search2DMatrix } from "./search-a-2d-matrix";
 import { twoSum } from "./two-sum";
 import { validParentheses } from "./valid-parentheses";
 
